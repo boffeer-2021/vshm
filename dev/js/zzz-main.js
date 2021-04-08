881521f80df92a913f46e435d019c890fba66b7d
boffeer <boffeechane@gmail.com>
2021-04-08 21:33:44 +0300

    before popups

---
+++ dev/js/zzz-main.js
@@ -0,0 +1,26 @@
+const syndromeSlider = new Swiper('.syndrome-slider', {
+  // loop: true,
+  navigation: {
+    nextEl: '.swiper-button-next.syndrome-button-next',
+    prevEl: '.swiper-button-prev.syndrome-button-prev',
+  },
+});
+const baseSlider1 = new Swiper('.base-slider-1', {
+  // loop: true,
+  navigation: {
+    nextEl: '.swiper-button-next.base-1-button-next',
+    prevEl: '.swiper-button-prev.base-1-button-prev',
+  },
+});
+const baseSlider2 = new Swiper('.base-slider-2', {
+  // loop: true,
+  navigation: {
+    nextEl: '.swiper-button-next.base-2-button-next',
+    prevEl: '.swiper-button-prev.base-2-button-prev',
+  },
+});
+
+
+popa({
+	pop
+})
