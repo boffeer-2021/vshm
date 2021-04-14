158f4a7b57b06a493c8a462eb0f425001f4b309d
boffeer <boffeechane@gmail.com>
2021-04-15 01:56:18 +0300

    mask + smoother scroll

--- dev/js/zzz-main.js
+++ dev/js/zzz-main.js
@@ -1,3 +1,4 @@
+
 let dateFormattingOptions = {
 	// era: 'long',
 	// year: 'numeric',
@@ -79,7 +80,11 @@ waitUntilPixels({
 
 
 
+let phoneInput = [...document.querySelectorAll('.input--phone')];
 
+phoneInput.map(input => {
+	Inputmask({mask: '+7 (999) 999-99-99'}).mask(input);
+})
 
 
 
@@ -624,6 +629,66 @@ popa({
 })
 
 
+
+// ==== Smooth scrolling
+var app = {
+    scrolled: 0,
+    newPosition: 0,
+    interval: null,
+    speed: 0,
+
+    scrollTo: function(el) {
+        var link = el.getAttribute('href').replace('#', ''),
+            anchor = document.getElementById(link);
+
+        var location = 0;
+        if (anchor.offsetParent) {
+            do {
+                location += anchor.offsetTop;
+                anchor = anchor.offsetParent;
+            } while (anchor);
+        }
+        location = location >= 0 ? location : 0;
+
+        this.animateScroll(location);
+        return false;
+    },
+
+    animateScroll: function(pos) {
+        document.documentElement.scrollTop = 1;
+        var element = (document.documentElement && document.documentElement.scrollTop) ? document.documentElement : document.body,
+            start = element.scrollTop,
+            change = pos - start,
+            currentTime = 0,
+            increment = 20,
+            duration = 300;
+
+        var animateScroll = function(){
+            currentTime += increment;
+            var val = Math.easeInOutQuad(currentTime, start, change, duration);
+            element.scrollTop = val;
+            if(currentTime < duration) {
+                setTimeout(animateScroll, increment);
+            }
+        };
+        animateScroll();
+    }
+};
+
+Math.easeInOutQuad = function (t, b, c, d) {
+    t /= d/2;
+    if (t < 1) return c/2*t*t + b;
+    t--;
+    return -c/2 * (t*(t-2) - 1) + b;
+};
+
+const anchors = [...document.querySelectorAll('a')]
+
+anchors.map(anchor => {
+	anchor.addEventListener('click', function() {
+		return app.scrollTo(this);
+	})
+})
 
 // popa({
 //   pop: '.pop-leaving',
